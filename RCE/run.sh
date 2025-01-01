467778e9b7d06f86b057f8173ce708e11b4455f5
victor <chrollolucilfer1402@gmail.com>
2025-01-01 21:24:09 +0530

    created a seperate docker file for prod and pushed to docker hub

--- RCE/run.sh
+++ RCE/run.sh
@@ -5,13 +5,21 @@ if [[ $1 == 'rabbitmq' ]]; then
 fi
 
 if [[ $1 == 'redis' ]]; then
-  docker run -d --name redis-db --network rce-network -p 6379:6379 -p 8001:8001 redis/redis-stack:latest
+  docker run -d --name redis-db --network rce-network -p 6379:6379 -p 8001:8001 redis/redis-stack:6.2.6-v17
 fi
 
 if [[ $1 == 'build' ]]; then
-  docker build -t rce .
+  docker build -t rce-dev -f Dockerfile.dev .
 fi
 
 if [[ $1 == 'run' ]]; then
-  docker run --network rce-network -v ./:/app -v /var/run/docker.sock:/var/run/docker.sock rce:latest
+  docker run --network rce-network -v ./:/app -v /var/run/docker.sock:/var/run/docker.sock rce-dev:latest
+fi
+
+if [[ $1 == 'build:prod' ]]; then
+  docker build -t rce-prod .
+fi
+
+if [[ $1 == 'run:prod' ]]; then
+  docker run --network rce-network -v ./temp_code:/app/temp_code -v /var/run/docker.sock:/var/run/docker.sock rce-prod:latest
 fi
